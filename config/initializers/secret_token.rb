0bba9bf36e640c4c8976e5e21126d380ec9d060a
hclub <ec2-user@ip-172-31-23-33.us-west-2.compute.internal>
2016-02-14 07:08:57 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hclub::Application.config.secret_key_base = 'c56c701e0db9982a1b2bae651f5e93ffd388e681f578aed01054d0509c124273fc2af22d825fc966054125664bc967fbbc51bba84a486324b980d3c439e683f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hclub::Application.config.session_store :cookie_store, key: '_hclub_session'
